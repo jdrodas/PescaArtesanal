2d2874c9e97324d0fbbd5259c97970c6a9a6a83b
Juan Dario Rodas <jdrodas@hotmail.com>
2023-02-28 07:19:58 -0500

    Agregado proyecto PescaArtesanal_WinForms

---
+++ PescaArtesanal_WindowsForms/PescaArtesanal_WindowsForms/AccesoDatos.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SQLite;
+using Dapper;
+using System.Configuration;
+
+namespace PescaArtesanal_WindowsForms
+{
+    public class AccesoDatos
+    {
+        /// <summary>
+        /// Obtiene la cadena de conexión a la DB a partir del archivo de configuración de la App
+        /// </summary>
+        private static string ObtenerCadenaConexion(string id)
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+
+        #region CRUD de Departamentos
+
+        /// <summary>
+        /// Obtiene una lista con los nombres de los Departamentos
+        /// </summary>
+        public static List<string> ObtieneListaNombreDepartamentos()
+        {
+            string cadenaConexion = ObtenerCadenaConexion("PescasDB");
+
+            using (IDbConnection cxnDB = new SQLiteConnection(cadenaConexion))
+            {
+                var salida = cxnDB.Query<string>("SELECT DISTINCT nombre FROM departamentos ORDER BY nombre", new DynamicParameters());
+                return salida.ToList();
+            }
+        }
+
+        #endregion CRUD de Departamentos
+
+        #region CRUD de Municipios
+
+        /// <summary>
+        /// Obtiene una lista con los nombres de los Municipios
+        /// </summary>
+        public static List<string> ObtieneListaNombreMuincipios()
+        {
+            string cadenaConexion = ObtenerCadenaConexion("PescasDB");
+
+            using (IDbConnection cxnDB = new SQLiteConnection(cadenaConexion))
+            {
+                var salida = cxnDB.Query<string>("SELECT DISTINCT nombre FROM municipios ORDER BY nombre", new DynamicParameters());
+                return salida.ToList();
+            }
+        }
+
+        #endregion CRUD de Municipios
+
+    }
+}
---
+++ PescaArtesanal_WindowsForms/PescaArtesanal_WindowsForms/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace PescaArtesanal_WindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstDepartamentos = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lstMunicipios = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(185, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(408, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pesca Artesanal en Colombia";
+            // 
+            // lstDepartamentos
+            // 
+            this.lstDepartamentos.FormattingEnabled = true;
+            this.lstDepartamentos.ItemHeight = 16;
+            this.lstDepartamentos.Location = new System.Drawing.Point(129, 101);
+            this.lstDepartamentos.Name = "lstDepartamentos";
+            this.lstDepartamentos.Size = new System.Drawing.Size(265, 84);
+            this.lstDepartamentos.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Departamentos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(406, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Municipios";
+            // 
+            // lstMunicipios
+            // 
+            this.lstMunicipios.FormattingEnabled = true;
+            this.lstMunicipios.ItemHeight = 16;
+            this.lstMunicipios.Location = new System.Drawing.Point(512, 101);
+            this.lstMunicipios.Name = "lstMunicipios";
+            this.lstMunicipios.Size = new System.Drawing.Size(265, 84);
+            this.lstMunicipios.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lstMunicipios);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lstDepartamentos);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pesca Artesanal";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstDepartamentos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lstMunicipios;
+    }
+}
+
