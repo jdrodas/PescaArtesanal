0a6b7d34ca4fdb3721581cb2830e2bc4224da6d1
Juan Dario Rodas <jdrodas@hotmail.com>
2023-04-18 06:12:25 -0500

    Commit inicial del proyecto con NoSQL en WindowsForms

---
+++ PescaArtesanal_NoSQL_WindowsForms/PescaArtesanal_NoSQL_WindowsForms/Form1.cs
@@ -0,0 +1,10 @@
+namespace PescaArtesanal_NoSQL_WindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
